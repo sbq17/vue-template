69ef7246f8bec7b65ae68725beb20b0d575071b2
heiyu17 <2010426591@qq.com>
2024-09-05 16:05:23 +0800

    增加配置信息

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,36 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(), vueJsx(), vueDevTools()],
+	plugins: [
+		vue(),
+		vueJsx(),
+		vueDevTools(),
+		AutoImport({
+			imports: [
+				'vue',
+				{
+					vue: ['createElementVNode']
+				},
+				{
+					from: 'vue',
+					imports: ['CSSProperties', 'SetupContext', 'ShallowRef'],
+					type: true
+				}
+			],
+			// ignore: ['h'],
+			resolvers: [],
+			dts: fileURLToPath(new URL('./types/auto-imports.d.ts', import.meta.url))
+		}),
+		Components({
+			resolvers: [],
+			dts: fileURLToPath(new URL('./types/components.d.ts', import.meta.url))
+		})
+	],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
